f3f128832e6c6ded80450d8e86968a6f9d13fcce
Diem Bang <diembang09@gmail.com>
2023-11-17 22:38:22 +0000

    Created folders, saved images and set up html and script files

---
+++ script.js
@@ -0,0 +1,35 @@
+
+/*
+
+Produktlista:
+- Skapa produktlista i en object/array
+- Skapa en plus och minusknapp för varje produkt
+- Plusknapp: När man klickar på plus ska varan läggas till i varukorgen och totalsumman ska uppdateras
+Start:
+Minusknapp: Varan ska dras bort från varukorgen och totalsumman ska uppdateras
+
+- Totalsumman ska vara 0 innan man har lagt till något
+
+Headern med varukorgsymbol och totalt pris:
+Varukorgsymbolen ska uppdateras när man lägger till eller ta bort vara
+Totalt pris ska uppdateras när man lägger till eller ta bort vara
+
+Produktmenyn:
+Produkterna ska gå att sortera utifrån namn, pris, kategori och rating
+
+Varukorgsammanställning: 
+Minus- och plusknapparna ska göra samma sak som ovan
+Krysset: Ta bort varan när man klickar
+Rabattkod: Visa kodfält när man klickar
+
+
+Specialregler - IF-satser:
+På måndagar innan kl. 10 ges 10 % rabatt på hela beställningssumman. Detta visas i varukorgssammanställningen som en rad med texten "Måndagsrabatt: 10 % på hela beställningen".
+På fredagar efter kl. 15 och fram till natten mellan söndag och måndag kl. 03.00 tillkommer ett helgpåslag på 15 % på alla munkar. Detta ska inte framgå för kunden att munkarna är dyrare, utan priset ska bara vara högre i "utskriften" av munkarna.
+Om kunden har beställt för totalt mer än 800 kr ska det inte gå att välja faktura som betalsätt.
+Om kunden har beställt minst 10 munkar av samma sort, ska munkpriset för just denna munksort rabatteras med 10 %
+Om kunden beställer totalt mer än 15 munkar så blir frakten gratis. I annat fall är fraktsumman 25 kr plus 10% av totalbeloppet i varukorgen.
+Om kunden inte har lagt beställningen inom 15 minuter så ska beställningsformuläret tömmas/rensas och kunden ska meddelas att denne är för långsam.
+
+
+*/
